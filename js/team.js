d4e69d913b2c6112b6176ba8d1afed4af851235b
Andrew Sillers <apsillers@gmail.com>
2012-09-05 02:20:42 -0400

    added more copyright + GPL notices

--- js/team.js
+++ js/team.js
@@ -1,3 +1,23 @@
+/*
+    Copyright (C) 2012 Andrew P. Sillers (apsillers@gmail.com)
+
+    This file is part of the HTML5 Netrek Client.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
 var IND = 0x0, FED = 0x1, ROM = 0x2, KLI = 0x4, ORI = 0x8;
 var teamLib = {
     IND:0x0,
