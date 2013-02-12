e34c6542d4d9398b09f81cb683e18f2ff42c8d5c
Andrew Sillers <apsillers@gmail.com>
2013-02-11 19:01:55 -0500

    Added license notices

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,5 +1,30 @@
 /*
-    packets.js should issue
+    Copyright (C) 2012 Andrew P. Sillers (apsillers@gmail.com)
+
+    This file is part of the HTML5 Netrek Client.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+    Singleton that handles the intro tutorial.
+
+    Various parts of the code (especially packets.js) call
+
+        tutorial.handleKeyword("someKeyword")
+
+    where "someKeyword" is the keyword indicating what the player just did (join, speed2, torp, etc.) so that the tutorial can respond to user action
 */
 tutorial = {
     active: false,
